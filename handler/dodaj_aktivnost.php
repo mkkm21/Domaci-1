9c3ab04e025de8c6cbee4e1d9b647fcac7c470ab
Mihajlo Kovacevic <kmihajlo21@gmail.com>
2022-11-17 22:07:12 +0100

    Dodato za naslov

--- handler/dodaj_aktivnost.php
+++ handler/dodaj_aktivnost.php
@@ -2,8 +2,8 @@
 require "../dbBroker.php";
 require "../model/aktivnosti.php";
 
-if(isset($_POST['opis'])){
-    $aktivnosti = new Aktivnosti(null, $_POST['opis'], $_POST['katID']);
+if(isset($_POST['naslov']) && isset($_POST['opis'])){
+    $aktivnosti = new Aktivnosti(null, $_POST['naslov'], $_POST['opis'], $_POST['user_id']);
     $status = Aktivnosti::dodaj_aktivnost($aktivnosti, $conn);
 
     if($status){
